bb0c4a5dc4efe7909a6566986a2dd80bc989040d
Zihadul Islam <zynscriptofficial@gmail.com>
2023-05-26 20:01:59 +0600

    footer  complete

--- src/pages/Shared/Footer.jsx
+++ src/pages/Shared/Footer.jsx
@@ -1,8 +1,35 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 const Footer = () => {
   return (
-    <h5 className="text-center my-5">&copy; Copyright 2023 Savory Secrets</h5>
+    <>
+      <div className="container my-5 border shadow rounded py-5 ">
+        <div className="footer-links mb-4 d-flex justify-content-center gap-4 fw-semibold">
+          <Link to="/">Home</Link>
+          <Link to="/blog">Blog</Link>
+          <Link to="/login">Login</Link>
+          <Link to="/register">Register</Link>
+          <Link to="#">About</Link>
+          <Link to="#">Contact</Link>
+        </div>
+        <div className="d-flex justify-content-center gap-2">
+          <input
+            className="p-2 rounded border shadow-sm"
+            type="email"
+            placeholder="Enter Your Email"
+          />
+          <button className="btn btn-outline-primary ">Subscribe</button>
+        </div>
+        <div className="fs-2 my-4 d-flex justify-content-center gap-3">
+          <i class="fa-brands fa-facebook"></i>
+          <i class="fa-brands fa-github"></i>
+          <i class="fa-brands fa-youtube"></i>
+          <i class="fa-brands fa-twitter"></i>
+        </div>
+        <h5 className="text-center">&copy; Copyright 2023 Savory Secrets</h5>
+      </div>
+    </>
   );
 };
 
